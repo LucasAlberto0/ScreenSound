af22c1bc409f0f631a391cbe7d75aa94d678de94
Lucas Alberto <lucasalberto4321@gmail.com>
2025-09-15 11:18:28 -0300

    exercise-1

--- Program.cs
+++ Program.cs
@@ -1,33 +1,46 @@
-﻿
-//1
-Console.WriteLine("Escreva um número: ");
-string valor = Console.ReadLine()!;
-int notaMedia = int.Parse(valor);
-
-
-if (notaMedia >= 5)
+﻿//1
+void operacoesBasicas()
 {
-    Console.WriteLine("Nota suficiente para aprovação!");
-}
-else
-{
-    Console.WriteLine("Reprovou!");
+    Console.WriteLine("1. Adição");
+    Console.WriteLine("2. Divisão");
+    Console.WriteLine("3. Subtração");
+    Console.WriteLine("4. Multiplicação");
+
 }
 
+operacoesBasicas();
 
-//2
+string escolha = Console.ReadLine()!;
+    int escolhaNumerica = int.Parse(escolha);
 
-List<string> linguagens = new List<string> { "C#", "Java", "JavaScript" };
+Console.WriteLine("Digite o primeiro número: ");
+
+string num11 = Console.ReadLine()!;
+double num1 = double.Parse(num11);
+
+Console.WriteLine("Digite o primeiro número: ");
+
+string num22 = Console.ReadLine()!;
+double num2 = double.Parse(num22);
+
+switch (escolhaNumerica)
+{
+    case 1:
+        Console.WriteLine("Sua adição deu: " + (num1 + num2));
+        break;
+    case 2:
+        Console.WriteLine("Sua Divisão deu: " + (num1 / num2));
+        break;
+    case 3:
+        Console.WriteLine("Sua Subtração deu: " + (num1 - num2));
+        break;
+    case 4:
+        Console.WriteLine("Sua Multiplicação deu: " + (num1 * num2));
+        break;
+    default:
+        Console.WriteLine("Opção Inválida!");
+        break;
+}
 
 
-//3
-
-Console.WriteLine(linguagens[0]);
-
-//4
-
-Console.WriteLine("Digite a posição desejada: ");
-int posicao = int.Parse(Console.ReadLine());
-
-Console.WriteLine(linguagens[posicao]);
 
