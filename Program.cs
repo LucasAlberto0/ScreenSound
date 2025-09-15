ec7bec22bf6462e5b6ad13c5052e30939924397c
Lucas Alberto <lucasalberto4321@gmail.com>
2025-09-15 10:47:53 -0300

    feat: for-and-list-sintaxe

--- Program.cs
+++ Program.cs
@@ -1,6 +1,7 @@
 ﻿string mensagemDeBoasVindas = "Boas vindas ao Screen Sound";
+List<string> listaDasBandas = new List<string>{"U2", "The Beatles", "Calipso"};
 
-void ExibirMensagemDeBoasVindas()
+void ExibirOLogo()
 {
     Console.WriteLine(@"
 ░██████╗░█████╗░██████╗░███████╗███████╗███╗░░██╗  ░██████╗░█████╗░██╗░░░██╗███╗░░██╗██████╗░
@@ -25,11 +26,10 @@ void ExibirOpcoesDoMenu()
     int opcaoEscolhidaNumerica = int.Parse(opcaoEscolhida);
     switch (opcaoEscolhidaNumerica)
     {
-        case 1:
+        case 1: RegistrarBandas();
             Console.WriteLine("Você escolheu a opção " + opcaoEscolhidaNumerica);
             break;
-        case 2:
-            Console.WriteLine("Você escolheu a opção " + opcaoEscolhidaNumerica);
+        case 2: MostrarBandasRegistradas();
             break;
         case 3:
             Console.WriteLine("Você escolheu a opção " + opcaoEscolhidaNumerica);
@@ -45,6 +45,36 @@ void ExibirOpcoesDoMenu()
     }
 }
 
-ExibirMensagemDeBoasVindas();
+void RegistrarBandas()
+{
+    Console.Clear();
+    Console.WriteLine("*************************");
+    Console.WriteLine("Registro de bandas");
+    Console.Write("Digite o nome da banda que deseja registrar: ");
+    string nomeDaBanda = Console.ReadLine()!;
+    listaDasBandas.Add(nomeDaBanda);
+    Console.WriteLine($"A banda  {nomeDaBanda} foi registrada com sucesso!");
+    Thread.Sleep(3000);
+    Console.Clear();
+    ExibirOpcoesDoMenu();
+}
+
+void MostrarBandasRegistradas()
+{
+    Console.Clear();
+    Console.WriteLine("*************************************");
+    Console.WriteLine("Exibindo todas as bandas registradas");
+    Console.WriteLine("**************************************\n");
+    for (int i = 0; i < listaDasBandas.Count; i++)
+    {
+        Console.WriteLine($"Banda: {listaDasBandas[i]}");
+    }
+    Console.WriteLine("\nDigite uma tecla para voltar ao menu principal");
+    Console.ReadKey();
+    Console.Clear();
+    ExibirOpcoesDoMenu();
+}
+
+ExibirOLogo();
 ExibirOpcoesDoMenu();
 
